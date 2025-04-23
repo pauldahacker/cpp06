3a9a9abfa187443db61183f6509e42dec1151cd8
pauldahacker <paul.demasca@gmail.com>
2025-04-23 12:51:43 +0200

    done for real

--- ex02/Identify.cpp
+++ ex02/Identify.cpp
@@ -26,11 +26,11 @@ Base	*generate(void)
 
 void	identify(Base* p)
 {
-	if (dynamic_cast<A *>(p))
+	if (dynamic_cast<A *>(p) != NULL)
 		std::cout << "p points to class A" << std::endl;
-	else if (dynamic_cast<B *>(p))
+	else if (dynamic_cast<B *>(p) != NULL)
 		std::cout << "p points to class B" << std::endl;
-	else if (dynamic_cast<C *>(p))
+	else if (dynamic_cast<C *>(p) != NULL)
 		std::cout << "p points to class C" << std::endl;
 	else
 		std::cout << "p points to none of A, B or C" << std::endl;
